632e2cebdf4b635a40bc422b58f1543c25470024
kothawleprem <kothawleprem@gmail.com>
2021-02-23 10:32:52 +0530

    improved location

--- location/views.py
+++ location/views.py
@@ -2,25 +2,32 @@ from django.shortcuts import render
 from geopy.geocoders import Nominatim
 from .utils import get_geo
 from requests import get
-import publicip
+from ipstack import GeoLookup
+import requests
+import json
+
+
 
 
 def for_location(request):
     geolocator = Nominatim(user_agent="location")
 
+    geo_lookup = GeoLookup("582179b24ab7f5f07d71c6fb0be3fc7c")
+    ulocation = geo_lookup.get_own_location()
+    print(ulocation)
     #ip = '192.168.0.105'
     #ip = get('https://api.ipify.org').text
     #print('My public IP address is: {}'.format(ip))
     #ip = ("",publicip.get(),"")
-    ip = get('http://ip.42.pl/raw').text
+    #ip = get('http://ip.42.pl/raw').text
     
-    country,city,lat,lon = get_geo(ip)
+    # country,city,lat,lon = get_geo(ip)
    
-    print('location country',country)
-    print('location city',city)
+    # print('location country',country)
+    # print('location city',city)
 
-    ulocation = geolocator.geocode(city)
-    print('###',ulocation)
+    # ulocation = geolocator.geocode(city)
+    # print('###',ulocation)
     context = {
         'ulocation' : ulocation,
     }
--- weather/urls.py
+++ weather/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from djweather import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('location.urls',namespace='location'))
+    #path('',views.home,name='home')
 ]
